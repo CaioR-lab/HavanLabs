db50106aeb19e63f26528aa8a04efd166247079d
caio <caio.mateus.r@gmail.com>
2021-07-13 10:38:54 -0300

    add web proj

---
+++ Web/Web/Controllers/UsuarioController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Data;
+
+namespace Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+       UsuarioRepository  repository = new UsuarioRepository();
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+
+            List<Usuario> model = repository.ReadAll();
+            return View(model);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Usuario model)
+        {
+            using (var context = new HavanLabsContext())
+            {
+                context.Usuarios.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("List");
+        }
+        public ActionResult Update(int id)
+        {
+            Usuario model = repository.ReadById (id);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Update(Usuario model)
+        {
+            repository.Update(model);
+            return RedirectToAction("List");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            repository.Delete(id);
+            return RedirectToAction("List");
+        }
+    }
+}
