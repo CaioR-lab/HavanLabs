455f6278358b1bab47ec615dc530440957cd83bc
caio <caio.mateus.r@gmail.com>
2021-07-13 08:32:27 -0300

    teste

---
+++ Data/UsuarioRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    class UsuarioRepository
+    {
+        //Create
+        public void create()
+        {
+
+        }
+        //ReadAll
+        public void ReadAll()
+        {
+
+        }
+        //ReadById
+        public void ReadById()
+        {
+
+        }
+        //Update
+        public void Update()
+        {
+
+        }
+        //Delete
+        public void Delete()
+        {
+
+        }
+    }
+}
